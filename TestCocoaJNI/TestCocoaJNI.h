2659d5a6f5cb841e614d527ffa02989ec8f3b6f5
TimmyOVO <timmyovo@qq.com>
2019-12-16 12:04:01 +0800

    Initial Commit

---
+++ TestCocoaJNI/TestCocoaJNI.h
@@ -0,0 +1,13 @@
+//
+//  TestCocoaJNI.h
+//  TestCocoaJNI
+//
+//  Created by OVO Timmy on 2019/12/16.
+//  Copyright © 2019 OVO Timmy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestCocoaJNI : NSObject
+
+@end
---
+++ TestCocoaJNI/TestCocoaJNI.m
@@ -0,0 +1,13 @@
+//
+//  TestCocoaJNI.m
+//  TestCocoaJNI
+//
+//  Created by OVO Timmy on 2019/12/16.
+//  Copyright © 2019 OVO Timmy. All rights reserved.
+//
+
+#import "TestCocoaJNI.h"
+
+@implementation TestCocoaJNI
+
+@end
